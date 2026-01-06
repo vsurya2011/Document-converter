011273a48d777f1aa0e9daac9a463b9a237df8e4
Surya V <krishnasurya2011@gmail.com>
2026-01-06 22:51:32 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-# Install the system tools needed for conversion
-apt-get update
-apt-get install -y libreoffice poppler-utils
